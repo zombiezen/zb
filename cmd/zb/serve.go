zombiezen/zb

commit 77f1732e5a0eb258caba95edb1ae89d44bc2d521

Increase `zbstorerpc` package encapsulation (#367)
- Use `zbstore.Importer` interface for receiving exports.
- Rename `zbstorerpc.Store` to `zbstorerpc.Client`.
- Add `zbstorerpc.Serve` function that handles closing the read side.
- Unexport `zbstorerpc.Codec` and split into separate codecs.
  The logic has bifurcated enough that this makes sense to do.
- Remove `zbstorerpc.DeferredImporter`.
- Move export ID plumbing into zbstorerpc.
- Simplify dependency injection in cmd/zb.
- Add tests to `internal/zbstorerpc`.

cmd/zb/serve.go
@@ -8,7 +8,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"net/netip"
@@ -26,7 +25,6 @@ import (
 	"golang.org/x/sync/errgroup"
 	"zb.256lights.llc/pkg/bytebuffer"
 	"zb.256lights.llc/pkg/internal/backend"
-	"zb.256lights.llc/pkg/internal/jsonrpc"
 	"zb.256lights.llc/pkg/internal/osutil"
 	"zb.256lights.llc/pkg/internal/system"
 	"zb.256lights.llc/pkg/internal/ui"
@@ -98,13 +96,18 @@ func (c *serveCommand) Run(ctx context.Context, g *globalConfig, drain drainSign
 	}
 	// TODO(someday): Properly set permissions on the created database.
 
-	configStoreDeps, cleanupStoreDeps := g.storeDeps()
-	defer cleanupStoreDeps()
-	fallbackStore, err := g.Server.Download.toStore(configStoreDeps)
+	provideHTTPClient, cleanup := singletonProvider(g.newHTTPClient)
+	defer cleanup()
+
+	fallbackStore, err := g.Server.Download.toStore(func() (zbstorehttp.Client, error) {
+		return provideHTTPClient()
+	})
 	if err != nil {
 		return err
 	}
-	uploadStore, err := g.Server.Upload.toStore(configStoreDeps)
+	uploadStore, err := g.Server.Upload.toStore(func() (zbstorehttp.Client, error) {
+		return provideHTTPClient()
+	})
 	if err != nil {
 		return err
 	}
@@ -270,29 +273,12 @@ func (c *serveCommand) listenRPC(ctx context.Context, server *backend.Server, g 
 		}()
 	}
 
-	openConns := make(sets.Set[net.Conn])
-	var openConnsMu sync.Mutex
 	ctx, cancel := context.WithCancel(ctx)
 	var grp sync.WaitGroup
 	defer func() {
 		cancel()
 		grp.Wait()
 	}()
-	grp.Add(1)
-	context.AfterFunc(ctx, func() {
-		// Once the context is Done, refuse new connections and RPCs.
-		defer grp.Done()
-		if err := l.Close(); err != nil {
-			log.Errorf(ctx, "Closing Unix socket: %v", err)
-		}
-		openConnsMu.Lock()
-		for conn := range openConns.All() {
-			if err := closeRead(conn); err != nil {
-				log.Errorf(ctx, "Closing Unix socket: %v", err)
-			}
-		}
-		openConnsMu.Unlock()
-	})
 	log.Infof(ctx, "Listening on %s", g.StoreSocket)
 
 	for {
@@ -300,9 +286,6 @@ func (c *serveCommand) listenRPC(ctx context.Context, server *backend.Server, g 
 		if err != nil {
 			return err
 		}
-		openConnsMu.Lock()
-		openConns.Add(conn)
-		openConnsMu.Unlock()
 
 		grp.Go(func() {
 			recv := server.NewNARReceiver(ctx, bytebuffer.TempFileCreator{
@@ -310,21 +293,15 @@ func (c *serveCommand) listenRPC(ctx context.Context, server *backend.Server, g 
 			})
 			defer recv.Cleanup(ctx)
 
-			codec := zbstorerpc.NewCodec(nopCloser{conn}, &zbstorerpc.CodecOptions{
-				Importer: zbstorerpc.NewReceiverImporter(recv),
-			})
-			err := jsonrpc.Serve(backend.WithExporter(ctx, codec), codec, server)
+			var serverImporter struct {
+				*backend.Server
+				zbstorerpc.Importer
+			}
+			serverImporter.Server = server
+			serverImporter.Importer = zbstorerpc.NewReceiverImporter(recv)
+			err := zbstorerpc.Serve(ctx, conn, serverImporter)
 			if err != nil {
 				log.Debugf(ctx, "Disconnecting from %v: %v", conn.RemoteAddr(), err)
-			}
-			codec.Close()
-
-			openConnsMu.Lock()
-			openConns.Delete(conn)
-			openConnsMu.Unlock()
-
-			if err := conn.Close(); err != nil {
-				log.Errorf(ctx, "%v", err)
 			}
 		})
 	}
@@ -482,20 +459,4 @@ func listenUnix(path string) (*net.UnixListener, error) {
 	}
 
 	return l, nil
-}
-
-func closeRead(c net.Conn) error {
-	cr, ok := c.(interface{ CloseRead() error })
-	if !ok {
-		return fmt.Errorf("%T does not support uni-directional close", c)
-	}
-	return cr.CloseRead()
-}
-
-type nopCloser struct {
-	io.ReadWriter
-}
-
-func (nopCloser) Close() error {
-	return nil
 }

cmd/zb/config.go
@@ -17,6 +17,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/auth/credentials"
@@ -31,7 +32,6 @@ import (
 	"zb.256lights.llc/pkg/internal/backend"
 	"zb.256lights.llc/pkg/internal/fileurl"
 	"zb.256lights.llc/pkg/internal/httpcache"
-	"zb.256lights.llc/pkg/internal/jsonrpc"
 	"zb.256lights.llc/pkg/internal/zbstorehttp"
 	"zb.256lights.llc/pkg/internal/zbstorerpc"
 	"zb.256lights.llc/pkg/zbstore"
@@ -290,7 +290,19 @@ func (g *globalConfig) reusePolicy() *zbstorerpc.ReusePolicy {
 	return &zbstorerpc.ReusePolicy{PublicKeys: g.TrustedPublicKeys}
 }
 
-func (g *globalConfig) newHTTPClient() (*httpClient, io.Closer, error) {
+func (g *globalConfig) newHTTPClient() (_ *httpClient, cleanup func(), err error) {
+	if err := os.MkdirAll(filepath.Dir(g.HTTPCacheDB), 0o777); err != nil {
+		return nil, nil, err
+	}
+	var netrcData []byte
+	if g.NetrcPath == "" {
+		var err error
+		netrcData, err = os.ReadFile(g.NetrcPath)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			return nil, nil, err
+		}
+	}
+
 	baseTransport := &http.Transport{
 		// Settings copied from [http.DefaultTransport].
 		Proxy: http.ProxyFromEnvironment,
@@ -305,10 +317,6 @@ func (g *globalConfig) newHTTPClient() (*httpClient, io.Closer, error) {
 		ExpectContinueTimeout: 1 * time.Second,
 	}
 	baseTransport.RegisterProtocol(althttp.GCSScheme, g.newGCSTransport(baseTransport))
-
-	if err := os.MkdirAll(filepath.Dir(g.HTTPCacheDB), 0o777); err != nil {
-		return nil, nil, err
-	}
 	cache := httpcache.Open(g.HTTPCacheDB, baseTransport, &httpcache.Options{
 		MaxResponseSize:         4 << 20, // 4 MiB
 		RequestCoalescingCutoff: 5 * time.Second,
@@ -320,22 +328,16 @@ func (g *globalConfig) newHTTPClient() (*httpClient, io.Closer, error) {
 			}
 		}),
 	})
-	client := &httpClient{
+	cleanup = func() {
+		if err := cache.Close(); err != nil {
+			log.Warnf(context.Background(), "%v", err)
+		}
+		baseTransport.CloseIdleConnections()
+	}
+	return &httpClient{
 		Transport: cache,
-	}
-	if g.NetrcPath == "" {
-		return client, cache, nil
-	}
-	netrcData, err := os.ReadFile(g.NetrcPath)
-	if errors.Is(err, os.ErrNotExist) {
-		return client, cache, nil
-	}
-	if err != nil {
-		cache.Close()
-		return nil, nil, fmt.Errorf("open netrc file: %v", err)
-	}
-	client.Netrc = netrcData
-	return client, cache, nil
+		Netrc:     netrcData,
+	}, cleanup, nil
 }
 
 func (g *globalConfig) newGCSTransport(baseRoundTripper http.RoundTripper) http.RoundTripper {
@@ -387,49 +389,10 @@ func (t *fileSplitTransport) RoundTrip(req *http.Request) (*http.Response, error
 	return t.fallback.RoundTrip(req)
 }
 
-func (g *globalConfig) storeClient(ctx context.Context, opts *zbstorerpc.CodecOptions) *jsonrpc.Client {
-	return jsonrpc.NewClient(ctx, func(ctx context.Context) (jsonrpc.ClientCodec, error) {
-		conn, err := (&net.Dialer{}).DialContext(ctx, "unix", g.StoreSocket)
-		if err != nil {
-			return nil, err
-		}
-		return zbstorerpc.NewCodec(conn, opts), nil
+func (g *globalConfig) openLocalStore(ctx context.Context) *zbstorerpc.Client {
+	return zbstorerpc.NewClient(ctx, func(ctx context.Context) (io.ReadWriteCloser, error) {
+		return (&net.Dialer{}).DialContext(ctx, "unix", g.StoreSocket)
 	})
-}
-
-func (g *globalConfig) storeDeps() (_ *storeDeps, cleanup func()) {
-	var state struct {
-		client       *httpClient
-		clientCloser io.Closer
-	}
-
-	deps := &storeDeps{
-		httpClientProvider: func() (*httpClient, error) {
-			if state.client == nil {
-				var err error
-				state.client, state.clientCloser, err = g.newHTTPClient()
-				if err != nil {
-					return state.client, err
-				}
-			}
-			return state.client, nil
-		},
-	}
-	cleanup = func() {
-		if state.client != nil {
-			state.client.CloseIdleConnections()
-		}
-		if state.clientCloser != nil {
-			if err := state.clientCloser.Close(); err != nil {
-				log.Warnf(context.Background(), "%v", err)
-			}
-		}
-	}
-	return deps, cleanup
-}
-
-type storeDeps struct {
-	httpClientProvider func() (*httpClient, error)
 }
 
 type storeConfigType string
@@ -578,7 +541,7 @@ func (sc *storeConfig) UnmarshalText(text []byte) error {
 	return nil
 }
 
-func (sc *storeConfig) toStore(deps *storeDeps) (backend.Store, error) {
+func (sc *storeConfig) toStore(provideHTTPClient func() (zbstorehttp.Client, error)) (backend.Store, error) {
 	if sc == nil {
 		return zbstore.Null{}, nil
 	}
@@ -590,7 +553,7 @@ func (sc *storeConfig) toStore(deps *storeDeps) (backend.Store, error) {
 		if err := jsonv2.Unmarshal(sc.properties, &props); err != nil {
 			return nil, fmt.Errorf("unmarshal http store configuration: %v", err)
 		}
-		client, err := deps.httpClientProvider()
+		client, err := provideHTTPClient()
 		if err != nil {
 			return nil, err
 		}
@@ -651,4 +614,28 @@ type storeConfigHTTPProperties struct {
 // defaultVarDir returns "/opt/zb/var/zb" on Unix-like systems or `C:\zb\var\zb` on Windows systems.
 func defaultVarDir() string {
 	return filepath.Join(filepath.Dir(string(zbstore.DefaultDirectory())), "var", "zb")
+}
+
+// singletonProvider returns a function that calls f at most once
+// and a cleanup function that calls any cleanup function returned from f.
+func singletonProvider[T any](f func() (T, func(), error)) (provider func() (T, error), cleanup func()) {
+	var state struct {
+		init    sync.Once
+		x       T
+		cleanup func()
+		err     error
+	}
+	provider = func() (T, error) {
+		state.init.Do(func() {
+			state.x, state.cleanup, state.err = f()
+		})
+		return state.x, state.err
+	}
+	cleanup = func() {
+		state.init.Do(func() {})
+		if state.cleanup != nil {
+			state.cleanup()
+		}
+	}
+	return provider, cleanup
 }

cmd/zb/derivation.go
@@ -83,22 +83,14 @@ func (c *derivationShowCommand) Run(ctx context.Context, g *globalConfig) error 
 		}
 	}
 
-	httpClient, httpCloser, err := g.newHTTPClient()
+	httpClient, cleanup, err := g.newHTTPClient()
 	if err != nil {
 		return err
 	}
-	defer func() {
-		httpClient.CloseIdleConnections()
-		if err := httpCloser.Close(); err != nil {
-			log.Warnf(ctx, "%v", err)
-		}
-	}()
-	di := new(zbstorerpc.DeferredImporter)
-	storeClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-		Importer: di,
-	})
+	defer cleanup()
+	storeClient := g.openLocalStore(ctx)
 	defer storeClient.Close()
-	eval, err := c.newEval(g, httpClient, storeClient, di)
+	eval, err := c.newEval(g, httpClient, storeClient)
 	if err != nil {
 		return err
 	}
@@ -311,22 +303,14 @@ func (c *derivationEnvCommand) Validate() error {
 }
 
 func (c *derivationEnvCommand) Run(ctx context.Context, g *globalConfig) error {
-	httpClient, httpCloser, err := g.newHTTPClient()
+	httpClient, cleanup, err := g.newHTTPClient()
 	if err != nil {
 		return err
 	}
-	defer func() {
-		httpClient.CloseIdleConnections()
-		if err := httpCloser.Close(); err != nil {
-			log.Warnf(ctx, "%v", err)
-		}
-	}()
-	di := new(zbstorerpc.DeferredImporter)
-	storeClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-		Importer: di,
-	})
+	defer cleanup()
+	storeClient := g.openLocalStore(ctx)
 	defer storeClient.Close()
-	eval, err := c.newEval(g, httpClient, storeClient, di)
+	eval, err := c.newEval(g, httpClient, storeClient)
 	if err != nil {
 		return err
 	}

cmd/zb/main.go
@@ -218,18 +218,14 @@ func (opts *evalOptions) Validate() error {
 	return nil
 }
 
-func (opts *evalOptions) newEval(g *globalConfig, httpClient frontend.HTTPClient, storeClient *jsonrpc.Client, di *zbstorerpc.DeferredImporter) (*frontend.Eval, error) {
-	store := &rpcStore{
-		dir:        g.Directory,
-		keepFailed: opts.KeepFailed,
-		Store: zbstorerpc.Store{
-			Handler: storeClient,
-		},
-		reuse: opts.reusePolicy(g),
-	}
-	di.SetImporter(store)
+func (opts *evalOptions) newEval(g *globalConfig, httpClient frontend.HTTPClient, localStore *zbstorerpc.Client) (*frontend.Eval, error) {
 	return frontend.NewEval(&frontend.Options{
-		Store:          store,
+		Store: &rpcStore{
+			dir:        g.Directory,
+			keepFailed: opts.KeepFailed,
+			Client:     localStore,
+			reuse:      opts.reusePolicy(g),
+		},
 		StoreDirectory: g.Directory,
 		CacheDBPath:    g.CacheDB,
 		HTTPClient:     httpClient,
@@ -262,22 +258,14 @@ func (c *evalCommand) Signature() string {
 }
 
 func (c *evalCommand) Run(ctx context.Context, g *globalConfig) error {
-	httpClient, httpCloser, err := g.newHTTPClient()
+	httpClient, cleanup, err := g.newHTTPClient()
 	if err != nil {
 		return err
 	}
-	defer func() {
-		httpClient.CloseIdleConnections()
-		if err := httpCloser.Close(); err != nil {
-			log.Warnf(ctx, "%v", err)
-		}
-	}()
-	di := new(zbstorerpc.DeferredImporter)
-	storeClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-		Importer: di,
-	})
+	defer cleanup()
+	storeClient := g.openLocalStore(ctx)
 	defer storeClient.Close()
-	eval, err := c.newEval(g, httpClient, storeClient, di)
+	eval, err := c.newEval(g, httpClient, storeClient)
 	if err != nil {
 		return err
 	}
@@ -314,22 +302,14 @@ func (c *buildCommand) Signature() string {
 }
 
 func (c *buildCommand) Run(ctx context.Context, g *globalConfig) error {
-	httpClient, httpCloser, err := g.newHTTPClient()
+	httpClient, cleanup, err := g.newHTTPClient()
 	if err != nil {
 		return err
 	}
-	defer func() {
-		httpClient.CloseIdleConnections()
-		if err := httpCloser.Close(); err != nil {
-			log.Warnf(ctx, "%v", err)
-		}
-	}()
-	di := new(zbstorerpc.DeferredImporter)
-	storeClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-		Importer: di,
-	})
+	defer cleanup()
+	storeClient := g.openLocalStore(ctx)
 	defer storeClient.Close()
-	eval, err := c.newEval(g, httpClient, storeClient, di)
+	eval, err := c.newEval(g, httpClient, storeClient)
 	if err != nil {
 		return err
 	}
@@ -473,7 +453,7 @@ func forceSymlink(oldname, newname string) error {
 // It copies builder logs to stderr
 // and propagates options from [evalOptions].
 type rpcStore struct {
-	zbstorerpc.Store
+	*zbstorerpc.Client
 	dir        zbstore.Directory
 	keepFailed bool
 	reuse      *zbstorerpc.ReusePolicy
@@ -481,7 +461,7 @@ type rpcStore struct {
 
 func (store *rpcStore) FetchObjects(ctx context.Context, paths []zbstore.Path) (map[zbstore.Path]*zbstorerpc.ObjectInfo, error) {
 	var resp zbstorerpc.FetchResponse
-	err := jsonrpc.Do(ctx, store.Handler, zbstorerpc.FetchMethod, &resp, &zbstorerpc.FetchRequest{
+	err := jsonrpc.Do(ctx, store, zbstorerpc.FetchMethod, &resp, &zbstorerpc.FetchRequest{
 		Paths: paths,
 	})
 	if err != nil {
@@ -492,7 +472,7 @@ func (store *rpcStore) FetchObjects(ctx context.Context, paths []zbstore.Path) (
 
 func (store *rpcStore) Realize(ctx context.Context, want sets.Set[zbstore.OutputReference]) ([]*zbstorerpc.BuildResult, error) {
 	var realizeResponse zbstorerpc.RealizeResponse
-	err := jsonrpc.Do(ctx, store.Handler, zbstorerpc.RealizeMethod, &realizeResponse, &zbstorerpc.RealizeRequest{
+	err := jsonrpc.Do(ctx, store, zbstorerpc.RealizeMethod, &realizeResponse, &zbstorerpc.RealizeRequest{
 		DrvPaths: slices.Collect(func(yield func(zbstore.Path) bool) {
 			for ref := range want.All() {
 				if !yield(ref.DrvPath) {
@@ -506,7 +486,7 @@ func (store *rpcStore) Realize(ctx context.Context, want sets.Set[zbstore.Output
 	if err != nil {
 		return nil, err
 	}
-	build, _, err := waitForBuild(ctx, store.Handler, realizeResponse.BuildID)
+	build, _, err := waitForBuild(ctx, store, realizeResponse.BuildID)
 	if err != nil {
 		return nil, err
 	}

cmd/zb/store.go
@@ -14,7 +14,6 @@ import (
 	"os"
 	"path/filepath"
 	"slices"
-	"strconv"
 	"strings"
 
 	"github.com/go-json-experiment/json/jsontext"
@@ -67,8 +66,8 @@ func (c *storeObjectInfoCommand) Signature() string {
 }
 
 func (c *storeObjectInfoCommand) Run(ctx context.Context, g *globalConfig) error {
-	storeClient := g.storeClient(ctx, nil)
-	defer storeClient.Close()
+	store := g.openLocalStore(ctx)
+	defer store.Close()
 
 	const errNotExist = "does not exist"
 
@@ -88,7 +87,7 @@ func (c *storeObjectInfoCommand) Run(ctx context.Context, g *globalConfig) error
 			var partialParsed struct {
 				Info jsontext.Value `json:"info"`
 			}
-			err = jsonrpc.Do(ctx, storeClient, zbstorerpc.InfoMethod, &partialParsed, req)
+			err = jsonrpc.Do(ctx, store, zbstorerpc.InfoMethod, &partialParsed, req)
 			if err != nil {
 				return fmt.Errorf("%s: %v", path, err)
 			}
@@ -106,7 +105,7 @@ func (c *storeObjectInfoCommand) Run(ctx context.Context, g *globalConfig) error
 		}
 
 		resp := new(zbstorerpc.InfoResponse)
-		err = jsonrpc.Do(ctx, storeClient, zbstorerpc.InfoMethod, resp, req)
+		err = jsonrpc.Do(ctx, store, zbstorerpc.InfoMethod, resp, req)
 		if err != nil {
 			return fmt.Errorf("%s: %v", path, err)
 		}
@@ -132,9 +131,9 @@ func (c *storeObjectInfoCommand) Run(ctx context.Context, g *globalConfig) error
 }
 
 type storeObjectExportCommand struct {
-	Paths             []string `kong:"arg,name=path"`
-	IncludeReferences bool     `kong:"name=references,negatable,help=Include referenced store objects (default ${default}),default=true"`
-	OutputPath        string   `kong:"name=output,short=o,placeholder=file,help=Output file"`
+	Paths             []zbstore.Path `kong:"arg,name=path"`
+	IncludeReferences bool           `kong:"name=references,negatable,help=Include referenced store objects (default ${default}),default=true"`
+	OutputPath        string         `kong:"name=output,short=o,placeholder=file,help=Output file"`
 }
 
 func (c *storeObjectExportCommand) Signature() string {
@@ -153,31 +152,15 @@ func (c *storeObjectExportCommand) Run(ctx context.Context, g *globalConfig) err
 	closer := xio.CloseOnce(output)
 	defer closer.Close()
 
-	toOutput := zbstorerpc.ImportFunc(func(header jsonrpc.Header, body io.Reader) error {
-		return zbstore.ReceiveExport(nopReceiver{}, io.TeeReader(body, output))
-	})
-	storeClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-		Importer: toOutput,
-	})
-	defer storeClient.Close()
+	store := g.openLocalStore(ctx)
+	defer store.Close()
 
-	req := &zbstorerpc.ExportRequest{
-		Paths:             make([]zbstore.Path, len(c.Paths)),
+	err = store.StoreExport(ctx, output, sets.Collect(slices.Values(c.Paths)), &zbstore.ExportOptions{
 		ExcludeReferences: !c.IncludeReferences,
-	}
-	for i, p := range c.Paths {
-		var err error
-		req.Paths[i], err = zbstore.ParsePath(p)
-		if err != nil {
-			return err
-		}
-	}
-	if err := jsonrpc.Do(ctx, storeClient, zbstorerpc.ExportMethod, nil, req); err != nil {
+	})
+	if err != nil {
 		return err
 	}
-
-	// The export message is sent before the RPC response, so if we received the response,
-	// the export is complete.
 	if err := closer.Close(); err != nil {
 		return err
 	}
@@ -198,8 +181,8 @@ func (c *storeObjectImportCommand) Signature() string {
 }
 
 func (c *storeObjectImportCommand) Run(ctx context.Context, g *globalConfig) error {
-	storeClient := g.storeClient(ctx, nil)
-	defer storeClient.Close()
+	store := g.openLocalStore(ctx)
+	defer store.Close()
 
 	inputPaths := c.Paths
 	if len(inputPaths) == 0 {
@@ -209,14 +192,14 @@ func (c *storeObjectImportCommand) Run(ctx context.Context, g *globalConfig) err
 		log.Infof(ctx, "Waiting for data on stdin...")
 	}
 
-	storePaths, err := catExports(ctx, storeClient, inputPaths)
+	storePaths, err := catExports(ctx, store, inputPaths)
 	if err != nil {
 		return err
 	}
 	ok := true
 	for _, path := range storePaths {
 		var exists bool
-		err := jsonrpc.Do(ctx, storeClient, zbstorerpc.ExistsMethod, &exists, &zbstorerpc.ExistsRequest{
+		err := jsonrpc.Do(ctx, store, zbstorerpc.ExistsMethod, &exists, &zbstorerpc.ExistsRequest{
 			Path: string(path),
 		})
 		if err != nil {
@@ -234,8 +217,8 @@ func (c *storeObjectImportCommand) Run(ctx context.Context, g *globalConfig) err
 }
 
 // catExports concatenates the exports from the given files into a single export
-// and sends it to the store connected via the given client.
-func catExports(ctx context.Context, client *jsonrpc.Client, exportFiles []string) ([]zbstore.Path, error) {
+// and sends it to the store connected via the [zbstore.Importer].
+func catExports(ctx context.Context, store zbstore.Importer, exportFiles []string) ([]zbstore.Path, error) {
 	// If there are no files, then no-op.
 	if len(exportFiles) == 0 {
 		return nil, nil
@@ -248,12 +231,6 @@ func catExports(ctx context.Context, client *jsonrpc.Client, exportFiles []strin
 			return nil, err
 		}
 		defer f.Close()
-		size := int64(-1)
-		if info, err := f.Stat(); err != nil {
-			log.Warnf(ctx, "Unable to get size of %s: %v", inputFileName(exportFiles[0]), err)
-		} else if info.Mode().IsRegular() {
-			size = info.Size()
-		}
 
 		// We still need to parse the export to determine store paths to confirm.
 		// If this fails, don't fail the overall operation.
@@ -270,7 +247,7 @@ func catExports(ctx context.Context, client *jsonrpc.Client, exportFiles []strin
 			ch <- rec.paths
 		}()
 
-		err = importToStore(ctx, client, io.TeeReader(f, pw), size)
+		err = store.StoreImport(ctx, io.TeeReader(f, pw))
 		pw.Close()
 		paths := <-ch
 		return paths, err
@@ -280,7 +257,7 @@ func catExports(ctx context.Context, client *jsonrpc.Client, exportFiles []strin
 	pr, pw := io.Pipe()
 	ch := make(chan error)
 	go func() {
-		err := importToStore(ctx, client, pr, -1)
+		err := store.StoreImport(ctx, pr)
 		pr.CloseWithError(err)
 		ch <- err
 		close(ch)
@@ -354,28 +331,6 @@ func (pr *passthroughReceiver) ReceiveNAR(trailer *zbstore.ExportTrailer) {
 	}
 }
 
-// importToStore sends the content of r to client as an application/zb-store-export message.
-// If size is non-negative, then it is used as the message's Content-Length header.
-func importToStore(ctx context.Context, client *jsonrpc.Client, r io.Reader, size int64) error {
-	generic, releaseConn, err := client.Codec(ctx)
-	if err != nil {
-		return err
-	}
-	defer releaseConn()
-	codec, ok := generic.(*zbstorerpc.Codec)
-	if !ok {
-		return fmt.Errorf("store connection is %T (want %T)", generic, (*zbstorerpc.Codec)(nil))
-	}
-
-	var header jsonrpc.Header
-	if size >= 0 {
-		header = make(jsonrpc.Header)
-		header.Set("Content-Length", strconv.FormatInt(size, 10))
-		r = io.LimitReader(r, size)
-	}
-	return codec.Export(header, r)
-}
-
 // exportPathRecorder is a [zbstore.NARReceiver] that records the store paths encountered.
 // It optionally passes through its methods to wrapped.
 type exportPathRecorder struct {
@@ -426,43 +381,41 @@ func (c *storeObjectCopyCommand) Run(ctx context.Context, g *globalConfig) error
 	base.Path = strings.TrimRight(base.Path, "/") + "/"
 	sourceConfig := c.Source.resolve(base)
 	destinationConfig := c.Destination.resolve(base)
-	storeDeps, cleanup := g.storeDeps()
-	defer cleanup()
 	paths := sets.Collect(slices.Values(c.Paths))
+	provideHTTPClient, cleanup := singletonProvider(g.newHTTPClient)
+	defer cleanup()
 
 	switch {
 	case sourceConfig.isNull() && !destinationConfig.isNull():
-		di := new(zbstorerpc.DeferredImporter)
-		localStoreClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-			Importer: di,
+		localStore := g.openLocalStore(ctx)
+		defer localStore.Close()
+		destinationStore, err := destinationConfig.toStore(func() (zbstorehttp.Client, error) {
+			return provideHTTPClient()
 		})
-		defer localStoreClient.Close()
-		localStore := &zbstorerpc.Store{Handler: localStoreClient}
-		di.SetImporter(localStore)
-		destinationStore, err := destinationConfig.toStore(storeDeps)
 		if err != nil {
 			return err
 		}
 		return storeCopy(ctx, destinationStore, localStore, paths)
 	case !sourceConfig.isNull() && destinationConfig.isNull():
-		di := new(zbstorerpc.DeferredImporter)
-		localStoreClient := g.storeClient(ctx, &zbstorerpc.CodecOptions{
-			Importer: di,
+		localStore := g.openLocalStore(ctx)
+		defer localStore.Close()
+		sourceStore, err := sourceConfig.toStore(func() (zbstorehttp.Client, error) {
+			return provideHTTPClient()
 		})
-		defer localStoreClient.Close()
-		localStore := &zbstorerpc.Store{Handler: localStoreClient}
-		di.SetImporter(localStore)
-		sourceStore, err := sourceConfig.toStore(storeDeps)
 		if err != nil {
 			return err
 		}
 		return storeCopy(ctx, localStore, sourceStore, paths)
 	case !sourceConfig.isNull() && !destinationConfig.isNull():
-		sourceStore, err := sourceConfig.toStore(storeDeps)
+		sourceStore, err := sourceConfig.toStore(func() (zbstorehttp.Client, error) {
+			return provideHTTPClient()
+		})
 		if err != nil {
 			return err
 		}
-		destinationStore, err := destinationConfig.toStore(storeDeps)
+		destinationStore, err := destinationConfig.toStore(func() (zbstorehttp.Client, error) {
+			return provideHTTPClient()
+		})
 		if err != nil {
 			return err
 		}

commit 7304fa9087cffcd79d50b4b428a4be58a2ae3271

Fix `zb serve` interrupt (#374)
Broken by #367.

cmd/zb/serve.go
@@ -16,7 +16,6 @@ import (
 	"path/filepath"
 	"runtime"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/coreos/go-systemd/v22/activation"
@@ -37,6 +36,7 @@ import (
 	"zombiezen.com/go/log"
 	"zombiezen.com/go/log/zstdlog"
 	"zombiezen.com/go/nix"
+	"zombiezen.com/go/xcontext"
 )
 
 const contentAddressTempFilePattern = "zb-ca-*"
@@ -189,7 +189,7 @@ func (c *serveCommand) Run(ctx context.Context, g *globalConfig, drain drainSign
 				// we want to interrupt the listen goroutines by returning a non-nil error.
 				return cmp.Or(err, drainFinished)
 			case <-grpCtx.Done():
-				return grpCtx.Err()
+				return nil
 			}
 		})
 	}
@@ -255,6 +255,7 @@ func (c *serveCommand) listenRPC(ctx context.Context, server *backend.Server, g 
 			return fmt.Errorf("systemd passed in %d sockets (want 1)", len(listeners))
 		}
 		l = listeners[0]
+		log.Infof(ctx, "Listening on systemd-provided socket")
 	} else {
 		if err := os.Remove(g.StoreSocket); err != nil && !errors.Is(err, os.ErrNotExist) {
 			return err
@@ -272,42 +273,46 @@ func (c *serveCommand) listenRPC(ctx context.Context, server *backend.Server, g 
 				log.Warnf(ctx, "Failed to clean up socket: %v", err)
 			}
 		}()
+		log.Infof(ctx, "Listening on %s", g.StoreSocket)
 	}
 
-	ctx, cancel := context.WithCancel(ctx)
-	var grp sync.WaitGroup
-	defer func() {
-		cancel()
-		grp.Wait()
-	}()
-	log.Infof(ctx, "Listening on %s", g.StoreSocket)
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			return err
-		}
-
-		grp.Go(func() {
-			var serverImporter struct {
-				*backend.Server
-				zbstorerpc.Importer
-			}
-			serverImporter.Server = server
-			serverImporter.Importer = &zbstore.BufferedImporter{
-				// Don't close the connection if an object cannot be written.
-				ObjectWriter: ignoreErrorsObjectWriter{server},
-				HashType:     nix.SHA256,
-				BufferCreator: bytebuffer.TempFileCreator{
-					Pattern: "zb-serve-receive-*.nar",
-				},
-			}
-			err := zbstorerpc.Serve(ctx, conn, serverImporter)
+	grp, ctx := errgroup.WithContext(ctx)
+	closer := xcontext.CloseWhenDone(ctx, l)
+	defer closer.Close()
+	grp.Go(func() error {
+		for {
+			conn, err := l.Accept()
 			if err != nil {
-				log.Debugf(ctx, "Disconnecting from %v: %v", conn.RemoteAddr(), err)
+				return err
 			}
-		})
-	}
+
+			grp.Go(func() error {
+				var serverImporter struct {
+					*backend.Server
+					zbstorerpc.Importer
+				}
+				serverImporter.Server = server
+				serverImporter.Importer = &zbstore.BufferedImporter{
+					// Don't close the connection if an object cannot be written.
+					ObjectWriter: ignoreErrorsObjectWriter{server},
+					HashType:     nix.SHA256,
+					BufferCreator: bytebuffer.TempFileCreator{
+						Pattern: "zb-serve-receive-*.nar",
+					},
+				}
+				err := zbstorerpc.Serve(ctx, conn, serverImporter)
+				// [zbstorerpc.Serve] always returns an error.
+				// A client disconnect should not cause the server to shut down,
+				// so we always return nil.
+				if err != nil {
+					log.Debugf(ctx, "Disconnecting from %v: %v", conn.RemoteAddr(), err)
+				}
+				return nil
+			})
+		}
+	})
+
+	return grp.Wait()
 }
 
 type ignoreErrorsObjectWriter struct {

go.mod
@@ -32,7 +32,7 @@ require (
 	zombiezen.com/go/nix v0.0.0-20250514174927-d97ab08b45de
 	zombiezen.com/go/sqlite v1.4.0
 	zombiezen.com/go/uritemplate v0.0.0-20230807232759-d66f0f659650
-	zombiezen.com/go/xcontext v1.1.0
+	zombiezen.com/go/xcontext v1.2.0
 )
 
 require (

go.sum
@@ -527,5 +527,5 @@ zombiezen.com/go/sqlite v1.4.0 h1:N1s3RIljwtp4541Y8rM880qgGIgq3fTD2yks1xftnKU=
 zombiezen.com/go/sqlite v1.4.0/go.mod h1:0w9F1DN9IZj9AcLS9YDKMboubCACkwYCGkzoy3eG5ik=
 zombiezen.com/go/uritemplate v0.0.0-20230807232759-d66f0f659650 h1:TSdjpPKXf/ZtcmZ4+CRhfxHjKRS36FvlDR61VQbYDpg=
 zombiezen.com/go/uritemplate v0.0.0-20230807232759-d66f0f659650/go.mod h1:4KGL4w5fAJq1F5wqhtfHj3QUVEaPjcmh55rIz22SYGA=
-zombiezen.com/go/xcontext v1.1.0 h1:ajQQf4edDi0yOF2jZTf+q+VmrEX/hJZWvimgutXgee4=
-zombiezen.com/go/xcontext v1.1.0/go.mod h1:bXPuLPkAwZShCRDueoyymB/Hjn9qt+S+rfkXqGIKcSc=
+zombiezen.com/go/xcontext v1.2.0 h1:b+dDsyWTIOzjZtmW1Buun2qcCL54KAhxuVlHj/1ihx0=
+zombiezen.com/go/xcontext v1.2.0/go.mod h1:Nh5fEPMzoaLnHSowgEJuQ3DhKaQfI25ST+tPGyNiAPU=

package.nix
@@ -24,7 +24,7 @@ buildGoModule {
 
   src = ./.;
 
-  vendorHash = "sha256-YHTIveON3H1m9Q6/IEeZnDNMIUPnobpJYb48vovrozU=";
+  vendorHash = "sha256-1jxLCnM+AxFSj4QMTz4osr6G9SrZEutkypNbYT5bdGc=";
   goSum = builtins.readFile ./go.sum;
 
   postInstall = ''